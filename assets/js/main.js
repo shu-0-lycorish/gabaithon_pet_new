fb05d2a15b59f58ae0d18c9d1186dc7c191486a2
shu_0 <94584063+shu-0-lycorish@users.noreply.github.com>
2023-06-23 05:09:04 +0900

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+let counter, btnAdd;
+let n;
+ 
+function addCount(){
+    n++;
+    counter.innerHTML = n;
+}
+ 
+window.addEventListener("load", ()=>{
+    // 起動時の処理
+    counter = document.getElementById("counter");
+    btnAdd = document.getElementById("btnAdd");
+    n = 0;
+ 
+    btnAdd.addEventListener("click", addCount);
+});
